405f95d61c6ce0ac8774099bbd90a39df6c9816b
xdevalex <alex720101@gmail.com>
2021-05-07 16:02:18 +0800

    commit 5

---
+++ 5.longest-palindromic-substring.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=5 lang=cpp
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (30.74%)
+ * Likes:    10663
+ * Dislikes: 686
+ * Total Accepted:    1.3M
+ * Total Submissions: 4.2M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "a"
+ * Output: "a"
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: s = "ac"
+ * Output: "a"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters (lower-case and/or upper-case),
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+       return s;
+    }
+};
+// @lc code=end
